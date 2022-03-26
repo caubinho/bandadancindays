5324ffb7744117286f88b28c2d0326f4f17fa010
Caubinho Pacheco <caubinho@gmail.com>
2022-03-26 18:33:12 +0100

    Second Commit

---
+++ components/Home.vue
@@ -0,0 +1,36 @@
+<!-- Please remove this file from your project -->
+<template>
+<div class="leading-normal tracking-normal text-white bg-black" style="font-family: 'Source Sans Pro', sans-serif;">
+ <div class="pt-10">
+      <div class="container px-3 mx-auto flex flex-wrap flex-col md:flex-row items-center">
+        <!--Left Col-->
+        <div class="flex flex-col w-full md:w-2/5 justify-center items-start text-center md:text-left">
+          <p class="uppercase tracking-loose w-full">Bem-Vindo!</p>
+          <h1 class="my-4 text-5xl font-bold leading-tight">
+            Fale conosco
+          </h1>
+          <p class="leading-normal text-2xl mb-8">
+            Acesse os links abaixo ou pelo whats.
+          </p>
+          <a target="_blank" href="https://www.facebook.com/dancindaysdemaringa">Instagram: /dancindaysdemaringa </a>
+          <a target="_blank" href="https://www.instagram.com/bandadancindays">Facebook: /dancindaysdemaringa </a>
+          <a target="_blank" href="https://www.youtube.com/user/dancinmga">Youtube: /dancinmga </a>
+
+
+
+
+        </div>
+        <!--Right Col-->
+        <div class="w-full md:w-3/5 py-6 text-center">
+          <img class="w-full md:w-4/5 z-50" src="~assets/img/bg.jpg" />
+        </div>
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
